21cb62d826625806dc0b6216fde000740a0764d3
Vi Tran <contact.vitran@gmail.com>
2022-01-29 21:26:38 +1100

    App, NavBar, Home, Movies components complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return (
+          <div>
+            <h2>Name: {movie.title}</h2>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre) => {
+                return <li>{genre}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
